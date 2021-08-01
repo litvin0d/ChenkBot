e2207d36e1a43feb97613446418b6a8a7e3f7826
Benitka <73627677+litvin0d@users.noreply.github.com>
2021-08-01 19:29:20 +0500

    Updating comments

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 # создание бота (токен бота указать в ковычках)
 bot = telebot.TeleBot('TOKEN', parse_mode=None)
 
-# настройка браузера для работы в headless режиме
+# настройка браузера для работы в автономном режиме
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
@@ -27,17 +27,18 @@ def get_screenshot(uid, url):
 # команда /start
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    # создание кнопок
+    # создание клавиатуры из кнопок выбора группы и расписания звонков
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     choose = types.KeyboardButton('Выбрать группу')
     bells = types.KeyboardButton('Расписание звонков')
     keyboard.add(choose, bells)
-    # приветствие и добавление кнопок в чат
+    # приветствие и добавление клавиатуры в чат
     bot.send_message(message.chat.id, 'Привет!\nЭтот бот позволит тебе быстро узнать актуальное расписание пар.', reply_markup=keyboard)
 
-# выбор группы
+# выбор группы и отправка скриншота
 @bot.message_handler(content_types=['text'])
 def groups(message):
+    # создаёт список групп в виде клавиатуры из кнопок, если был выбран вариант 'Выбрать группу'
     if message.text == 'Выбрать группу':
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         gr1_20 = types.KeyboardButton('ТТО 1-20')
@@ -49,10 +50,13 @@ def groups(message):
         gr7_20 = types.KeyboardButton('ИСП 7-20')
         gr8_20 = types.KeyboardButton('ИСП 8-20')
         gr18_20 = types.KeyboardButton('ИСП 18-20')
+        # создаёт кнопку 'назад' для возвращения к клавиатуре 'выбор группы' и 'расписание звонков'
         back = types.KeyboardButton('Назад')
         keyboard.add(gr1_20, gr2_20, gr3_20, gr4_20, gr5_20, gr6_20, gr7_20, gr8_20, gr18_20, back)
+        # добавляет кнопки в чат
         bot.send_message(message.chat.id, 'Выбери группу:', reply_markup=keyboard)
-        
+   
+    # отправляет нужный скриншот в зависимости от выбранной группы
     elif message.text == 'ТТО 1-20':
         uid = message.chat.id
         url = 'https://pronew.chenk.ru/blocks/manage_groups/website/view.php?gr=246&dep=3'
@@ -89,12 +93,15 @@ def groups(message):
         uid = message.chat.id
         url = "https://pronew.chenk.ru/blocks/manage_groups/website/view.php?gr=254&dep=3"
         get_screenshot(uid, url)
+    # возвращает клавиатуру 'Выбор группы' и 'Расписание звонков'
     elif message.text == 'Назад':
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         choose = types.KeyboardButton('Выбрать группу')
         bells = types.KeyboardButton('Расписание звонков')
         keyboard.add(choose, bells)
         bot.send_message(message.chat.id, 'Выбери действие:', reply_markup=keyboard)
+        
+    # отправляет фотографию расписания звоноков    
     elif message.text == 'Расписание звонков':
         bot.send_photo(message.chat.id, photo=open('bells.png', 'rb'))
     
