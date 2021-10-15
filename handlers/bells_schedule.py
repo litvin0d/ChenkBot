bbd06c4f2dfd711420c46f71291b8e8b54b7d5ed
litvin0d <litvinovd944@gmail.com>
2021-10-16 00:14:42 +0500

    changed bells_schedule, added readme file

--- handlers/bells_schedule.py
+++ handlers/bells_schedule.py
@@ -1,21 +1,10 @@
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import Message
 
 from loader import bot, dp
 
 
-@dp.message_handler(text=['⏳ Расписание звонков ⏳', '⏳ Понедельник-пятница ⏳', '⏳ Суббота ⏳'])
+@dp.message_handler(text=['⏳ Расписание звонков ⏳'])
 async def bells_schedule(message: Message):
     if message.text == '⏳ Расписание звонков ⏳':
-        keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-        monday_friday = KeyboardButton('⏳ Понедельник-пятница ⏳')
-        saturday = KeyboardButton('⏳ Суббота ⏳')
-        back = KeyboardButton('🔙 Назад 🔙')
-        keyboard.add(monday_friday, saturday, back)
-        await message.answer('Какой день недели?', reply_markup=keyboard)
-        await message.answer('<b>Важно:</b> бот не учитывает изменения в расписании звонков.')
-
-    if message.text == '⏳ Понедельник-пятница ⏳':
         await bot.send_photo(message.chat.id, photo=open('img/monday_friday.jpeg', 'rb'))
-
-    if message.text == '⏳ Суббота ⏳':
         await bot.send_photo(message.chat.id, photo=open('img/saturday.jpeg', 'rb'))
--- utils/screenshot.py
+++ utils/screenshot.py
@@ -6,7 +6,7 @@ options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(executable_path=r'C:/Program Files (x86)/ChromeDriver/chromedriver.exe', options=options)
 driver.set_window_size(1000, 1000)
 
 
